92f88189e34dfa1527881a1c83f138f5c8c5f177
junhongGenesislab <unique3801@gmail.com>
2021-01-08 08:32:02 +0900

    update timeline

--- src/App.js
+++ src/App.js
@@ -20,6 +20,38 @@ function App() {
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
           <p>
+            -1 Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+            <a href="https://devjunhong.github.io/rust/backend_rust_rocket_react/">Rust and React</a>
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            0 Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+            <a href="https://devjunhong.github.io/rust/backend_rust_rocket_react/">Rust and React</a>
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
             1 Creative Direction, User Experience, Visual Design, Project Management, Team Leading
             <a href="https://devjunhong.github.io/rust/backend_rust_rocket_react/">Rust and React</a>
           </p>
